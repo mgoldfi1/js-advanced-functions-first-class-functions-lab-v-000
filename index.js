96f21c7bf16e76c3d5a3b44df213a0d4f7696aa5
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-07 13:41:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,4 @@
 
 const returnFirstTwoDrivers = function (drivers) {return drivers.slice(0,2)}
 const returnLastTwoDrivers = function (drivers) {return drivers.slice(-2)}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
