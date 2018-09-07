bc3f390f0a95cca10540ea3797dd4bad30543cbb
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-07 13:42:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,6 @@
 const returnFirstTwoDrivers = function (drivers) {return drivers.slice(0,2)}
 const returnLastTwoDrivers = function (drivers) {return drivers.slice(-2)}
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+function createFareMultiplier(integer) {
+  return function (num) {return num*integer}
+}
